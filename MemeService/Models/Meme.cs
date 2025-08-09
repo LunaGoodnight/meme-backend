1ed27f36fefab3e775ec8e3aaaa0746e38c2c99b
Luna <javaspringtea@gmail.com>
2025-08-09 23:14:17 +0800

    init

---
+++ MemeService/Models/Meme.cs
@@ -0,0 +1,9 @@
+﻿namespace MemeService.Models;
+
+public class Meme
+{
+    public int Id { get; set; }
+    public required string ImageUrl { get; set; }
+    public DateTime CreatedAt { get; set; }
+    public List<string> Keywords { get; set; } = new List<string>(); 
+}
