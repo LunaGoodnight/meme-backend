c8bcae20ba1bda7fca9f5a7b5bf05f06b79cd7fc
Luna <javaspringtea@gmail.com>
2025-08-10 23:09:30 +0800

    rm unused code

--- MemeService/Program.cs
+++ MemeService/Program.cs
@@ -3,7 +3,7 @@ using Amazon.S3;
 var builder = WebApplication.CreateBuilder(args);
 
 // Add services to the container.
-builder.Services.AddControllersWithViews();
+builder.Services.AddControllers();
 builder.Services.AddSingleton<IAmazonS3>(s =>
     new AmazonS3Client("SPACES_KEY", "SPACES_SECRET", new AmazonS3Config
     {
@@ -12,25 +12,9 @@ builder.Services.AddSingleton<IAmazonS3>(s =>
     }));
 var app = builder.Build();
 
-// Configure the HTTP request pipeline.
-if (!app.Environment.IsDevelopment())
-{
-    app.UseExceptionHandler("/Home/Error");
-    // The default HSTS value is 30 days. You may want to change this for production scenarios, see https://aka.ms/aspnetcore-hsts.
-    app.UseHsts();
-}
-
 app.UseHttpsRedirection();
 app.UseRouting();
 
-app.UseAuthorization();
-
-app.MapStaticAssets();
-
-app.MapControllerRoute(
-        name: "default",
-        pattern: "{controller=Home}/{action=Index}/{id?}")
-    .WithStaticAssets();
-
+app.MapControllers(); // Enables attribute routing for your API controllers
 
 app.Run();
